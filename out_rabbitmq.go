83db410027d62dcae098af803767ef22697dc198
Bfrn <bjoern.franke@mni.thm.de>
2019-12-31 03:40:44 +0100

    Implemented Routing-Key-Extraction

--- out_rabbitmq.go
+++ out_rabbitmq.go
@@ -2,25 +2,25 @@ package main
 
 import (
 	"C"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"log"
+	"regexp"
+	"strconv"
+	"strings"
 	"unsafe"
 
 	"github.com/fluent/fluent-bit-go/output"
 	"github.com/streadway/amqp"
 )
-import (
-	"encoding/json"
-	"errors"
-	"fmt"
-	"regexp"
-	"strings"
-)
 
 var (
-	connection *amqp.Connection
-	channel    *amqp.Channel
-	topicName  string
-	routingKey string
+	connection          *amqp.Connection
+	channel             *amqp.Channel
+	topicName           string
+	routingKey          string
+	routingKeyDelimiter string
 )
 
 //export FLBPluginRegister
@@ -40,8 +40,14 @@ func FLBPluginInit(plugin unsafe.Pointer) int {
 	topicName = output.FLBPluginConfigKey(plugin, "TopicName")
 	topicType := output.FLBPluginConfigKey(plugin, "TopicType")
 	routingKey = output.FLBPluginConfigKey(plugin, "RoutingKey")
+	routingKeyDelimiter = output.FLBPluginConfigKey(plugin, "RoutingKeyDelimiter")
 
-	err = routingKeyIsValid(routingKey)
+	if len(routingKeyDelimiter) < 1 {
+		routingKeyDelimiter = "."
+		logInfo("The routing-key-delimiter is set to the default value '" + routingKeyDelimiter + "' ")
+	}
+
+	err = routingKeyIsValid(routingKey, routingKeyDelimiter)
 	if err != nil {
 		logError("The Parsing of the Routing-Key failed: ", err)
 		return output.FLB_ERROR
@@ -96,7 +102,6 @@ func FLBPluginFlushCtx(ctx, data unsafe.Pointer, length C.int, tag *C.char) int 
 		}
 
 		timestamp := ts.(output.FLBTime)
-		println(timestamp.String())
 
 		parsedRecord := parseRecord(record)
 
@@ -109,11 +114,17 @@ func FLBPluginFlushCtx(ctx, data unsafe.Pointer, length C.int, tag *C.char) int 
 			continue
 		}
 
+		rk, err := createRoutingKey(routingKey, parsedRecord, routingKeyDelimiter)
+		if err != nil {
+			logError("Couldn't create the Routing-Key", err)
+			continue
+		}
+
 		err = channel.Publish(
-			topicName,  // exchange
-			routingKey, // routing key
-			false,      // mandatory
-			false,      // immediate
+			topicName, // exchange
+			rk,        // routing key
+			false,     // mandatory
+			false,     // immediate
 			amqp.Publishing{
 				ContentType: "application/json",
 				Body:        jsonString,
@@ -121,7 +132,6 @@ func FLBPluginFlushCtx(ctx, data unsafe.Pointer, length C.int, tag *C.char) int 
 		if err != nil {
 			logError("Couldn't publish record: ", err)
 		}
-		println(string(jsonString))
 	}
 	return output.FLB_OK
 }
@@ -129,9 +139,6 @@ func FLBPluginFlushCtx(ctx, data unsafe.Pointer, length C.int, tag *C.char) int 
 //export FLBPluginExit
 func FLBPluginExit() int {
 	return output.FLB_OK
-}
-
-func main() {
 }
 
 func parseRecord(mapInterface map[interface{}]interface{}) map[string]interface{} {
@@ -143,6 +150,8 @@ func parseRecord(mapInterface map[interface{}]interface{}) map[string]interface{
 			parsedMap[k.(string)] = string(t)
 		case map[interface{}]interface{}:
 			parsedMap[k.(string)] = parseRecord(t)
+		case []interface{}:
+			parsedMap[k.(string)] = parseSubRecordArray(t)
 		default:
 			parsedMap[k.(string)] = v
 		}
@@ -150,7 +159,23 @@ func parseRecord(mapInterface map[interface{}]interface{}) map[string]interface{
 	return parsedMap
 }
 
-func routingKeyIsValid(rk string) error {
+func parseSubRecordArray(arr []interface{}) []interface{} {
+	for idx, i := range arr {
+		switch t := i.(type) {
+		case []byte:
+			arr[idx] = string(t)
+		case map[interface{}]interface{}:
+			arr[idx] = parseRecord(t)
+		case []interface{}:
+			arr[idx] = parseSubRecordArray(t)
+		default:
+			arr[idx] = t
+		}
+	}
+	return arr
+}
+
+func routingKeyIsValid(rk string, rkDelimiter string) error {
 	r, err := regexp.Compile(`^\$((\[\"([^\s\"]+)\"\])|(\[\'([^\s\']+)\'\])|(\[[1-9][0-9]*\])|(\[[0]\]))+$`)
 	if err != nil {
 		return err
@@ -160,8 +185,8 @@ func routingKeyIsValid(rk string) error {
 		return errors.New("Routing-Key shouldn't be empty")
 	}
 
-	if strings.Contains(rk, ".") {
-		splittedRk := strings.Split(rk, ".")
+	if strings.Contains(rk, rkDelimiter) {
+		splittedRk := strings.Split(rk, rkDelimiter)
 		if arrayContainsString(splittedRk, "") {
 			return errors.New("The given routing-key contains an empty value")
 		}
@@ -182,8 +207,107 @@ func routingKeyIsValid(rk string) error {
 	return nil
 }
 
-func createRoutingKey(record map[string]interface{}) {
+func createRoutingKey(rk string, record map[string]interface{}, rkDelimiter string) (string, error) {
+	recordAccessorRegex, err := regexp.Compile(`(\'[^\s\']+\')|(\"[^\s\"]+\")|(\[0\])|(\[[1-9][0-9]*\])`)
+	if err != nil {
+		return "", err
+	}
 
+	var builder strings.Builder
+	recordAccessors := strings.Split(rk, rkDelimiter)
+
+	for idx, recordAccessor := range recordAccessors {
+		if strings.HasPrefix(recordAccessor, "$") {
+			subRk, err := extractValueFromRecord(record, recordAccessorRegex.FindAllString(recordAccessor, -1))
+			if err != nil {
+				return "", err
+			}
+			builder.WriteString(subRk)
+		} else {
+			builder.WriteString(recordAccessor)
+		}
+
+		if idx != (len(recordAccessors) - 1) {
+			builder.WriteString(rkDelimiter)
+		}
+	}
+
+	return builder.String(), nil
+}
+
+func extractValueFromRecord(record map[string]interface{}, keys []string) (string, error) {
+
+	if len(keys) > 0 {
+		arrKey := []rune(keys[0])
+		currentKey := string(arrKey[1:(len(arrKey) - 1)])
+
+		if strings.HasPrefix(keys[0], "[") {
+			return "", fmt.Errorf("Couldn't access the Record with the array-accessor '%s', record-accessor is required", currentKey)
+		}
+
+		val, recordContainsKey := record[currentKey]
+		if len(keys) == 1 {
+			if recordContainsKey {
+				return fmt.Sprintf("%v", val), nil
+			}
+			return "", fmt.Errorf("Can't access the record with the given record-accessor '%s'", currentKey)
+		}
+
+		subRecord, recordContainsSubRecord := val.(map[string]interface{})
+		if recordContainsSubRecord {
+			return extractValueFromRecord(subRecord, keys[1:])
+		}
+
+		recordArray, recordContainsArray := val.([]interface{})
+		if recordContainsArray {
+			return extractValueFromArray(recordArray, keys[1:])
+		}
+
+		return "", fmt.Errorf("Couldn't access the Record with the record-accessor '%s'", currentKey)
+	}
+
+	return "", fmt.Errorf("The given routing-key doesn't contain any values")
+}
+
+func extractValueFromArray(arr []interface{}, keys []string) (string, error) {
+
+	if len(keys) > 0 {
+		arrKey := []rune(keys[0])
+		currentKey := string(arrKey[1:(len(arrKey) - 1)])
+		idx, err := strconv.Atoi(currentKey)
+
+		if err != nil {
+			return "", fmt.Errorf("Couldn't parse the array-accessor '%s' to int", currentKey)
+		}
+
+		if strings.HasPrefix(keys[0], "\"") || strings.HasPrefix(keys[0], "'") {
+			return "", fmt.Errorf("Couldn't access the array with the record-accessor '%s', array-accessor is required", currentKey)
+		}
+
+		if len(arr) <= idx {
+			return "", fmt.Errorf("The given index '%s' exceededs the array-size", currentKey)
+		}
+
+		val := arr[idx]
+
+		if len(keys) == 1 {
+			return fmt.Sprintf("%v", val), nil
+		}
+
+		subRecord, recordContainsSubRecord := val.(map[string]interface{})
+		if recordContainsSubRecord {
+			return extractValueFromRecord(subRecord, keys[1:])
+		}
+
+		recordArray, recordContainsArray := val.([]interface{})
+		if recordContainsArray {
+			return extractValueFromArray(recordArray, keys[1:])
+		}
+
+		return "", fmt.Errorf("Couldn't access the Record with the record-accessor '%s'", currentKey)
+	}
+
+	return "", fmt.Errorf("The given routing-key doesn't contain any values")
 }
 
 func logInfo(msg string) {
@@ -201,4 +325,7 @@ func arrayContainsString(arr []string, str string) bool {
 		}
 	}
 	return false
+}
+
+func main() {
 }
